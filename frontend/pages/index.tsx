63578ed63b382fbb456e821af8be75fd3e31a7f6
kayukovzakhar <146358900+kayukovzakhar@users.noreply.github.com>
2025-06-04 12:45:20 +0300

    Update index.tsx
    изменение надписи на главной

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -80,7 +80,7 @@ export default function Home() {
       <Sidebar />
       <main className="flex-1 py-12 px-10 relative">
         <h1 className="text-4xl font-bold text-center mb-6 text-blue-900 drop-shadow">
-          Путешествия
+          Любимые маршруты у вас в телефоне
         </h1>
 
         {/* Поисковая строка */}
